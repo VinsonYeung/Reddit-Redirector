8b912513aa486c5da8891282f173caf29bcd6e23
Vinson Yeung <vinsonyeungvy@gmail.com>
2019-12-13 16:25:17 +1000

    Extension redirects form "www" to "old".

---
+++ background.js
@@ -0,0 +1,12 @@
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+        let host = "https://old.reddit.com";
+        let path = new URL(details.url).pathname;
+        return {redirectUrl: host + path}; 
+    },
+    {
+        urls: [
+            "*://www.reddit.com/*",
+        ]
+    },   ["blocking"]
+);
